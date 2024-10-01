afe7f96d0e26162d531fed3663dc30b54e1442a8
Ricardo <rcabete@debian.home>
2024-10-01 09:50:34 +0100

    Add alias, dbeaver installation and IDEA desktop icon to the script

--- install.sh
+++ install.sh
@@ -5,5 +5,30 @@ sudo apt update
 sudo apt -y full-upgrade
 sudo apt autoremove
 
+
+# Alias
+echo 'alias ll="ls -l"
+alias la="ls -la"
+alias cls=\'clear && echo -en "\e[3J"\'' > /home/coder/.bashrc
+
+
 # Install nano
 sudo apt install nano
+# Install dbeaver-ce
+sudo dpkg -i /home/coder/Downloads/dbeaver-ce_24.2.1_amd64.deb
+
+
+# Create IntelliJ IDEA CE desktop icon
+echo '[Desktop Entry]
+Version=1.0
+Type=Application
+Name=IntelliJ IDEA Community Edition
+Icon=/opt/idea/bin/idea.svg
+Exec="/opt/idea/bin/idea.sh" %f
+Comment=Capable and Ergonomic IDE for JVM
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-idea-ce
+StartupNotify=true' > /home/coder/.local/share/applications/jetbrains-idea-ce.desktop
+
+
